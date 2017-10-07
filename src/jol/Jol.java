3769c233f954a1e865fa9db317f7dd0d1e1e281a
oscar <oscar@MI-NOTEBOOK-OSCAR>
2017-10-07 17:12:27 +0200

    Oscar: introduits metodes suma, resta y mutiplicacio

--- src/jol/Jol.java
+++ src/jol/Jol.java
@@ -5,6 +5,8 @@
  */
 package jol;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Besso
@@ -15,7 +17,26 @@ public class Jol {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int A,B;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Inserte valor para A: ");
+        A = in.nextInt();
+        System.out.print("Inserte valor para B: ");
+        B = in.nextInt(); 
+        in.nextLine();
+        suma(A,B);
+        resta(A,B);
+        multiplicacion(A,B);
+        System.out.print("Pulsa ENTER per terminar... ");
+         in.nextLine();
     }
-    
+    public static void suma(int A,int B){
+        System.out.format("A+B= %4d%n", A+B);
+    }
+    public static void resta(int A,int B){
+        System.out.format("A-B= %4d%n", A-B);
+    }
+    public static void multiplicacion(int A,int B){
+    System.out.format("A*B= %4d%n", A*B);
+    }
 }
