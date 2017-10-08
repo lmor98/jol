4c46f3518dcf8aea2b38463a824f3a93b63f9322
Besso <Besso@LAPTOP-DR2VIURC>
2017-10-08 16:57:25 +0200

    Commit fecha laia

---
+++ src/jol/Fecha.java
@@ -0,0 +1,70 @@
+package jol;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Besso
+ */
+public class Fecha {
+    public static int dia;
+    public static int mes;
+    public static int anyo;
+    
+    public static void main(String[] args) {
+        String fecha;
+        Scanner in = new Scanner(System.in);
+        System.out.print("tu fecha es: ");
+    }
+    
+    private static boolean esBisiesto(){
+        return ((anyo % 4 == 0) && (anyo % 100 != 0) || (anyo % 400 == 0));
+    }
+    public static void setDia(int d) {
+        dia = d;
+    }
+    public static void setMes(int m){
+        mes = m;
+    }
+    public static void setAño(int a){
+        anyo = a;
+    }
+    public static void asignarFecha(int d, int m, int a){
+        setDia(d);
+        setMes(m);
+        setAño(a);
+    }
+    public static int getDia(){
+        return dia;
+    }
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAño() {
+        return anyo;
+    }
+    public static String fantasma(){
+        boolean diaCorrecto, mesCorrecto, anyoCorrecto;
+        anyoCorrecto = (anyo >0);
+        mesCorrecto = (mes >=1) && (mes <=12);
+        switch(mes){
+            case 2:
+                if(esBisiesto()){
+                    diaCorrecto = (dia >= 1 && dia <= 29);
+                }else{
+                    diaCorrecto = (dia >= 1 && dia <= 28);
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                diaCorrecto = (dia >= 1 && dia <= 30);
+                break;
+            default:
+                diaCorrecto = (dia >= 1 && dia <= 31);
+        }
+        return diaCorrecto + " / " + mesCorrecto + " / " + anyoCorrecto;
+    }
+    
+}
